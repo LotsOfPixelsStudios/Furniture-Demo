551e2f92f50fa897c87611816004b8e51e3430cc
jukplays <julian.kalmbach@gmail.com>
2023-07-01 20:10:18 +0200

    changed modern chair to specific resource selection to have another demonstration of it

--- src/main/kotlin/com/tcreative/addons/furniture/TableLamp.kt
+++ src/main/kotlin/com/tcreative/addons/furniture/TableLamp.kt
@@ -2,12 +2,14 @@ package com.tcreative.addons.furniture
 
 import com.tcreative.devtools.stdlib.furnitures.furniture
 import com.tcreative.devtools.stdlib.furnitures.lamp
+import com.tcreative.devtools.tranclate.builder.getResource
 import com.tcreative.devtools.tranclate.systemaddon.Addon
 
-class TableLamp(val addon: Addon) {
+class TableLamp(addon: Addon) {
     init {
         lamp("table_lamp", "Table Lamp", addon) {
-
+            geometry = getResource("furniture-resources/modern_lamp/modern_lamp1.geo.json")
+            texture = getResource("furniture-resources/modern_lamp/modern_lamp1.png")
         }
     }
 }
